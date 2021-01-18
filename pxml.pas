15c81be9d210e0fd1b4ea633aff746fb3702e9c8
garbagememo <66831242+garbagememo@users.noreply.github.com>
2021-01-18 22:02:10 +0900

    XMLファイルによるシーン読み書き

--- pxml.pas
+++ pxml.pas
@@ -1,6 +1,6 @@
-program pXML;
+﻿program pXML;
 {$mode objfpc}{$H+}
-uses SysUtils,Classes,uXML,uModel;
+uses SysUtils,Classes,uXML,uModel,uVect;
 
 var
    SR  : SnapRecord;
@@ -8,8 +8,9 @@ var
    spl : TList;
    cam : CameraRecord;
    pSt : string;
+  i:integer;
 BEGIN
-   InitScene;
+   InitScene;//SR.InitSceneにするべき？
    SR.MakeSnap;
    pSt:='xml';
    If Not DirectoryExists(pSt) then
@@ -17,8 +18,13 @@ BEGIN
 	 Writeln ('Failed to create directory !')
       else
 	 Writeln ('Created "NewDir" directory');
+  for i:=0 to sc.count-1 do begin
+    cam.Setup(CreateVec(50,52,295.6),CreateVec(0,-0.042612,-1),640,480,0.5135,140);
+     ScR.Cam:=cam;
+    ScR.spl:=TList(sc[i]);
+    WriteXMLScene(ScR,pst+'/'+IntToStr(i)+'Model.xml');
+  end;
    Sr.GetNextScene(640,480);
-   SR.CurSceneRec.Cam.SetSamples(16);
    writeXMLScene(SR.CurSceneRec,pSt+'/'+'teste.xml');
    writeln(' Write XML');
    ScR:=ReadXMLConf(pSt+'/'+'teste.xml');
